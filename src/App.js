23578fcfb5f9b8cd15c4c9a247e42d60608067a8
gomina maryam <gominamaryam@gmail.com>
2022-01-11 17:07:16 +0100

    added and setup react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
+import { Routes, Route } from "react-router-dom";
+
 import Header from "./components/Header";
+import Completed from "./pages/Completed";
+import Error from "./pages/Error";
+import Home from "./pages/Home";
+import Pending from "./pages/Pending";
 
 function App() {
   return (
     <>
       <Header/>
+      <Routes>
+        <Route exact path='/' element={<Home/>}/>
+        <Route path='*' element={<Error/>}/>
+        <Route path='/complete' element={<Completed/>}/>
+        <Route path='/pending' element={<Pending/>}/>
+      </Routes>
     </>
   );
 }
